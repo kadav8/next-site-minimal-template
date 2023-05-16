f99103930f92034fa4c3708e62d212cb4c454c04
kadav8 <kadav8@gmail.com>
2023-05-16 22:25:46 +0200

    Update page.js

--- app/[lang]/[column]/[slug]/page.js
+++ app/[lang]/[column]/[slug]/page.js
@@ -3,7 +3,7 @@ export const metadata = {
 };
 
 export default async function ArticlePage({ params }) {
-  const res = await fetch('/api/articles/10', { next: { revalidate: 1000 } });
+  const res = await fetch('https://next-site-minimal-template.vercel.app/api/articles/10', { next: { revalidate: 1000 } });
   const data = res.json();
 
   return (
